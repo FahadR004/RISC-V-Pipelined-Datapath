23b6403df8d686329771e29d46f2feab200707a8
FahadR004 <fahad.rasheed.004@gmail.com>
2025-10-28 01:34:40 +0500

    Worked on Fetch Stage

--- RISC_V_Pipeline.v
+++ RISC_V_Pipeline.v
@@ -5,13 +5,24 @@ module RISC_V_Pipeline (
     input reset
 );
 
-    Fetch_Stage fetch();
+    Fetch_Stage fetch(
+        .clk(clk),
+        
+    );
+
+    IF_ID IF_ID();
     
     Decode_Stage decode();
+
+    ID_EX ID_EX();
     
     Execute_Stage execute();
+
+    EX_MEM EX_MEM();
     
     Memory_Stage memory();
+
+    MEM_WB MEM_WB();
     
     WriteBack_Stage writeback();
 
---
+++ components/Immediate_Generator.v
@@ -0,0 +1,10 @@
+module Immediate_Generator #(
+    parameter data_width = 32
+) (
+    input clk,
+    input [data_width-1:0] address
+);
+
+
+    
+endmodule
